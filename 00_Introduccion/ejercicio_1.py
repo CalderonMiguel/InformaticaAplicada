802543e5ba03ccfa5b92c397f0391fd9005f3e51
Miguel <36174268@uaz.edu.mx>
2023-03-25 16:11:09 -0600

    Update example

--- 00_Introduccion/ejercicio_1.py
+++ 00_Introduccion/ejercicio_1.py
@@ -1,31 +1,37 @@
 #Ejercicios 1
-#ESTO ES UNA PRUEBAAAAAA222!!!3333
+#Prueba py 
+#------------------------------------------
+#------------ Informacion -----------------
+#Calderon Hernandez Miguel de Jesus 6to B
+# Matricula: 36174268
+#Materia: Informatica Aplicada
+#------------------------------------------
 # Introducción
 # Una expresión puede ser una operación aritmetica
 
-print(6 + 9)                       # 15
-print(3 - 1)                       # 
-print(1 * 1)                       # 
-print(8 / 5)                       # 
-print(2 ** 4)                      # 
-print(8 % 3)                       # 
-print(9 // 4)                      # 
-print(15<6)                        # 
-print(2.5 * 1.5 * 3)               # 
-print(2j + 2 + 3j + 5)             # 
-print('Hola mundo')                # 
-print(['Mar', 2, 5])               # 
-print({'Nombre':'Tu nombre'})      #
-print({'Algo', 2.67, 9.5})         # 
-print(('Lag', 6.8, 7.5, 9))        # 
+print(7  + 16)                       # 23
+print(34 - 14)                       # 20
+print(10 * 12)                       # 120
+print(81 /  9)                       # 9.0
+print(  2 ** 8)                      # 256
+print( 38 % 5)                       # 3
+print(92 // 43)                      # 2
+print(154<46)                        # False
+print(2.51 * 1.53 * 3)               # 11.5209
+print(2j + 42 + 3j + 5j)             # 42+10j
+print('Hola Informatica Aplicada')                # Hola Informatica Aplicada
+print(['Sommer', 2, 5])                           # ['Sommer',2,5]
+print({'Nombre':'Calderon'})                      #{'Nombre':'Calderon'}
+print({'5ta Estacion', 2.67, 9.5,'Prueba'})       # {9.5,'Prueba','5ta Estacion',2.67}
+print(('El sol no regresa', 6.8, 7.5, 9))         # ('El sol no regresa',6.8,7.5,9)
 
 # Tipos de datos
 
 print(type(15<6))                       # <class 'bool'>
-print(type(2.5 * 1.5 * 3))              #
-print(type(2j + 2 + 3j + 5))            #
-print(type('Hola mundo'))               # 
-print(type(['Mar', 2, 5]))            # 
-print(type({'Nombre':'Tu nombre'}))     # 
-print(type({'Algo', 2.67, 9.5}))        # 
-print(type(('Lag', 6.8, 7.5, 9)))       # 
+print(type(2.5 * 1.5 * 3))              # <class 'float'>
+print(type(2j + 2 + 3j + 5))            # <class 'complex'>
+print(type('Hola mundo'))               # <class 'str'>
+print(type(['Mar', 2, 5]))              # <class 'list'>
+print(type({'Nombre':'Tu nombre'}))     # <class 'dict'>
+print(type({'Algo', 2.67, 9.5}))        # <class 'set'>
+print(type(('Lag', 6.8, 7.5, 9)))       # <class 'tuple'>
